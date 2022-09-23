97dbbe9a3946695963797d48c99677b40fca2314
u120022 <u120022@st.pu-toyama.ac.jp>
2022-09-23 03:39:22 +0000

    fmt

--- public/analyzer.js
+++ public/analyzer.js
@@ -11,63 +11,63 @@ const [getActiveUnit, setHint, diagnostics] = arguments;
   // 卒業要件をもとにしたルール
   {
     const tag = "卒業条件";
-    macro(1, 2, 2, { tag, message: "総合科目 人間から 2単位必要です。" });
-    macro(3, 13, 6, { tag, message: "総合科目 社会・環境から 6単位必要です。" });
-    macro(14, 22, 4, { tag, message: "総合科目 言語・文化から 4単位必要です。" });
-    macro(23, 30, 3, { tag, message: "総合科目 精神・身体から 3単位必要です。" });
-    macro(1, 30, 19, { tag, message: "総合科目から 19単位必要です。" });
-    macro(31, 41, 13, { tag, message: "基礎科目から 13単位必要です。" });
-    macro(42, 53, 10, { tag, message: "外国語科目 英語から 10単位必要です。" });
-    macro(54, 57, 2, { tag, message: "外国語科目 第2外国語から 2単位必要です。" });
-    macro(58, 67, 7, { tag, message: "キャリア形成科目から 7単位必要です。" });
-    macro(68, 114, 79, { tag, message: "専門科目から 79単位必要です。" });
-		diagnostics.tagArray.push(tag);
+    macro(1, 2, 2, { tag, message: "総合科目人間から2単位必要です。" });
+    macro(3, 13, 6, { tag, message: "総合科目社会・環境から6単位必要です。" });
+    macro(14, 22, 4, { tag, message: "総合科目言語・文化から4単位必要です。" });
+    macro(23, 30, 3, { tag, message: "総合科目精神・身体から3単位必要です。" });
+    macro(1, 30, 19, { tag, message: "総合科目から19単位必要です。" });
+    macro(31, 41, 13, { tag, message: "基礎科目から13単位必要です。" });
+    macro(42, 53, 10, { tag, message: "外国語科目英語から10単位必要です。" });
+    macro(54, 57, 2, { tag, message: "外国語科目第2外国語から2単位必要です。" });
+    macro(58, 67, 7, { tag, message: "キャリア形成科目から7単位必要です。" });
+    macro(68, 114, 79, { tag, message: "専門科目から79単位必要です。" });
+    diagnostics.tagArray.push(tag);
   }
 
   // 卒業研究2履修条件をもとにしたルール
   {
     const tag = "卒業研究2履修条件";
-    macro(1, 2, 2, { tag, message: "卒業研究2の履修に 総合科目 人間から 2単位必要です。" });
-    macro(3, 13, 6, { tag, message: "卒業研究2の履修に 総合科目 社会・環境から 6単位必要です。" });
-    macro(14, 22, 4, { tag, message: "卒業研究2の履修に 総合科目 言語・文化から 4単位必要です。" });
-    macro(23, 30, 3, { tag, message: "卒業研究2の履修に 総合科目 精神・身体から 3単位必要です。" });
-    macro(1, 30, 15, { tag, message: "卒業研究2の履修に 卒業研究2履修に 総合科目から 15単位必要です。" });
-    macro(31, 41, 13, { tag, message: "卒業研究2の履修に 基礎科目から 13単位必要です。" });
-    macro(42, 53, 6, { tag, message: "卒業研究2の履修に 外国語科目 英語から 6単位必要です。" });
-    macro(54, 57, 2, { tag, message: "卒業研究2の履修に 外国語科目 第2外国語から 2単位必要です。" });
-    macro(58, 67, 3, { tag, message: "卒業研究2の履修に キャリア形成科目から 3単位必要です。" });
-    macro(68, 114, 69, { tag, message: "卒業研究2の履修に 専門科目 から69単位必要です。" });
-		diagnostics.tagArray.push(tag);
+    macro(1, 2, 2, { tag, message: "卒業研究2の履修に総合科目人間から2単位必要です。" });
+    macro(3, 13, 6, { tag, message: "卒業研究2の履修に総合科目社会・環境から6単位必要です。" });
+    macro(14, 22, 4, { tag, message: "卒業研究2の履修に総合科目言語・文化から4単位必要です。" });
+    macro(23, 30, 3, { tag, message: "卒業研究2の履修に総合科目精神・身体から3単位必要です。" });
+    macro(1, 30, 15, { tag, message: "卒業研究2の履修に総合科目から15単位必要です。" });
+    macro(31, 41, 13, { tag, message: "卒業研究2の履修に基礎科目から13単位必要です。" });
+    macro(42, 53, 6, { tag, message: "卒業研究2の履修に外国語科目英語から6単位必要です。" });
+    macro(54, 57, 2, { tag, message: "卒業研究2の履修に外国語科目第2外国語から2単位必要です。" });
+    macro(58, 67, 3, { tag, message: "卒業研究2の履修にキャリア形成科目から3単位必要です。" });
+    macro(68, 114, 69, { tag, message: "卒業研究2の履修に専門科目から69単位必要です。" });
+    diagnostics.tagArray.push(tag);
   }
 
   // 指定科目履修条件をもとにしたルール
   {
     const tag = "プレゼンテーション1と卒業研究1履修条件";
-    macro(1, 114, 70, { tag, message: "プレゼンテーション演習と卒業研究1の履修に すべての科目 から70単位必要です。" });
-		diagnostics.tagArray.push(tag);
+    macro(1, 114, 70, { tag, message: "プレゼンテーション演習と卒業研究1の履修にすべての科目から70単位必要です。" });
+    diagnostics.tagArray.push(tag);
   }
 
   // 備考をもとにしたルール
   {
     const tag = "備考";
-    macro(3, 13, 2, { tag, message: "経済学I-環境論II から2単位必要です。" });
-    macro(15, 22, 2, { tag, message: "コミュニケーション論-国際関係論 から2単位必要です。" });
-    macro(24, 30, 2, { tag, message: "心理学I-健康科学II から2単位必要です。" });
-    macro(35, 37, 2, { tag, message: "化学I-生物学 から2単位必要です。" });
-    macro(39, 40, 1, { tag, message: "数学物理学演習II-化学実験 から1単位必要です。" });
-    macro(42, 45, 4, { tag, message: "英語基礎1-英語基礎4 から4単位必要です。" });
-    macro(46, 53, 6, { tag, message: "総合英語1-英語特別演習4 から6単位必要です。" });
-    macro(54, 57, 2, { tag, message: "ドイツ語I-中国語II から2単位必要です。" });
-    macro(68, 75, 10, { tag, message: "卒業研究履修に 線形代数1-フーリエ解析学 から10単位必要です。" });
-    macro(80, 82, 4, { tag, message: "卒業研究2の履修に アルゴリズムとデータ構造-ディジタル信号処理 から4単位必要です。" });
-    macro(83, 85, 4, { tag, message: "卒業研究2の履修に 電気信号-論理回路基礎 から4単位必要です。" });
-    macro(91, 95, 5, { tag, message: "プログラミング2-IoTプログラミング から5単位必要です。" });
-    macro(96, 100, 6, { tag, message: "コンピュータアーキテクチャ-プログラミング言語 から6単位必要です。" });
-    macro(101, 104, 4, { tag, message: "コンピュータネットワーク-待ち行列理論と性能評価 から4単位必要です。" });
-    macro(105, 107, 2, { tag, message: "データベースとデータ処理-オペレーティングシステム から2単位必要です。" });
-    macro(108, 110, 2, { tag, message: "画像処理基礎-生体情報工学 から2単位必要です。" });
-    macro(111, 114, 4, { tag, message: "機械学習-情報システムと地球環境 から4単位必要です。" });
-		diagnostics.tagArray.push(tag);
+    macro(3, 13, 2, { tag, message: "経済学I-環境論IIから2単位必要です。" });
+    macro(15, 22, 2, { tag, message: "コミュニケーション論-国際関係論から2単位必要です。" });
+    macro(24, 30, 2, { tag, message: "心理学I-健康科学IIから2単位必要です。" });
+    macro(35, 37, 2, { tag, message: "化学I-生物学から2単位必要です。" });
+    macro(39, 40, 1, { tag, message: "数学物理学演習II-化学実験から1単位必要です。" });
+    macro(42, 45, 4, { tag, message: "英語基礎1-英語基礎4から4単位必要です。" });
+    macro(46, 53, 6, { tag, message: "総合英語1-英語特別演習4から6単位必要です。" });
+    macro(54, 57, 2, { tag, message: "ドイツ語I-中国語IIから2単位必要です。" });
+    macro(68, 75, 10, { tag, message: "卒業研究履修に線形代数1-フーリエ解析学 から10単位必要です。" });
+    macro(80, 82, 4, { tag, message: "卒業研究2の履修にアルゴリズムとデータ構造-ディジタル信号処理から4単位必要です。" });
+    macro(83, 85, 4, { tag, message: "卒業研究2の履修に電気信号-論理回路基礎から4単位必要です。" });
+    macro(91, 95, 5, { tag, message: "プログラミング2-IoTプログラミングから5単位必要です。" });
+    macro(96, 100, 6, { tag, message: "コンピュータアーキテクチャ-プログラミング言語から6単位必要です。" });
+    macro(101, 104, 4, { tag, message: "コンピュータネットワーク-待ち行列理論と性能評価から4単位必要です。" });
+    macro(105, 107, 2, { tag, message: "データベースとデータ処理-オペレーティングシステムから2単位必要です。" });
+    macro(108, 110, 2, { tag, message: "画像処理基礎-生体情報工学から2単位必要です。" });
+    macro(111, 114, 4, { tag, message: "機械学習-情報システムと地球環境から4単位必要です。" });
+    diagnostics.tagArray.push(tag);
   }
 }
 
@@ -114,7 +114,7 @@ const [getActiveUnit, setHint, diagnostics] = arguments;
     macro(88, { tag, message: "必須科目です。" });
     macro(89, { tag, message: "必須科目です。" });
     macro(90, { tag, message: "必須科目です。" });
-		diagnostics.tagArray.push(tag);
+    diagnostics.tagArray.push(tag);
   }
 
   {
@@ -126,6 +126,6 @@ const [getActiveUnit, setHint, diagnostics] = arguments;
     macro(87, { tag, message: "卒業研究2の履修に必要です。" });
     macro(88, { tag, message: "卒業研究2の履修に必要です。" });
     macro(89, { tag, message: "卒業研究2の履修に必要です。" });
-		// diagnostics.tagArray.push(tag);
+    // diagnostics.tagArray.push(tag);
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,217 +1,223 @@
 import {
-	Accessor,
-	Component,
-	createEffect,
-	createSignal,
-	For,
-	Index,
-	Show,
+  Accessor,
+  Component,
+  createEffect,
+  createSignal,
+  For,
+  Index,
+  Show,
 } from "solid-js";
 import { TableProvider, useTable } from "./TableProvider";
-import { HiOutlinePlus, HiOutlinePlusCircle, HiSolidCursorClick, HiSolidSearch } from "solid-icons/hi";
+import {
+  HiOutlinePlus,
+  HiOutlinePlusCircle,
+  HiSolidCursorClick,
+  HiSolidSearch,
+} from "solid-icons/hi";
 import { Course, Hint } from "./TableLoader";
 
 const CourseTableRow: Component<{
-	course: Accessor<Course>;
-	tagSet: Accessor<Set<string>>;
-	point: Accessor<number>;
-	onClick: () => void;
+  course: Accessor<Course>;
+  tagSet: Accessor<Set<string>>;
+  point: Accessor<number>;
+  onClick: () => void;
 }> = (props) => {
-	const [course] = createSignal<Course>(props.course());
-	const [has, setHas] = createSignal<boolean>(props.course().has);
-	const [hints, setHints] = createSignal<Hint[]>(props.course().hints);
+  const [course] = createSignal<Course>(props.course());
+  const [has, setHas] = createSignal<boolean>(props.course().has);
+  const [hints, setHints] = createSignal<Hint[]>(props.course().hints);
 
-	createEffect(() => {
-		const _has = props.course().has;
-		setHas(_has);
-		const _hints = props
-			.course()
-			.hints.filter((hint) => props.tagSet().has(hint.tag));
-		setHints(_hints);
-	});
+  createEffect(() => {
+    const _has = props.course().has;
+    setHas(_has);
+    const _hints = props
+      .course()
+      .hints.filter((hint) => props.tagSet().has(hint.tag));
+    setHints(_hints);
+  });
 
-	const Hints: Component = () => (
-		<>
-			{/* Sign */}
-			<div class="rounded-lg bg-purple-100 px-2 text-center">
-				<HiSolidSearch class="inline" color="#444444" />
-				{hints().length}
-			</div>
+  const Hints: Component = () => (
+    <>
+      {/* Sign */}
+      <div class="rounded-lg bg-purple-100 px-2 text-center">
+        <HiSolidSearch class="inline" color="#444444" />
+        {hints().length}
+      </div>
 
-			{/* Popover */}
-			<div class="pointer-events-none absolute top-full z-10 opacity-0 group-hover:opacity-100">
-				<div class="mt-2 rounded-lg border bg-slate-50 p-2">
-					<ul class="list-inside list-decimal">
-						<Index each={hints()}>
-							{(hint) => (
-								<li class="whitespace-nowrap">
-									{hint().message}({hint().tag})
-								</li>
-							)}
-						</Index>
-					</ul>
-				</div>
-			</div>
-		</>
-	);
+      {/* Popover */}
+      <div class="pointer-events-none absolute top-full z-10 opacity-0 group-hover:opacity-100">
+        <div class="mt-2 rounded-lg border bg-slate-50 p-2">
+          <ul class="list-inside list-decimal">
+            <Index each={hints()}>
+              {(hint) => (
+                <li class="whitespace-nowrap">
+                  {hint().message}({hint().tag})
+                </li>
+              )}
+            </Index>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 
-	return (
-		<tr
-			class={
-				has()
-					? "border-b bg-slate-100"
-					: "border-b"
-			}
-		>
-			<td class="group relative p-2">
-				<Show when={0 < hints().length}>
-					<Hints />
-				</Show>
-			</td>
-			<td class="p-2 text-center cursor-pointer" onClick={props.onClick} tabindex={0}><HiOutlinePlusCircle class="inline" color="#444444" />{course().name}</td>
-			<td class="p-2 text-center">{course().groups.get(1)}</td>
-			<td class="p-2 text-center">{course().groups.get(2)}</td>
-			<td class="p-2 text-center">{course().unit}</td>
-			<For each={Array.from(course().levels)}>
-				{([key, level]) => (
-					<td
-						class={(() => {
-							if (level) {
-								if (props.point() == key) {
-									return "p-2 text-center font-bold text-purple-600";
-								} else if (props.point() < key) {
-									return "p-2 text-center text-purple-600";
-								} else {
-									return "p-2 text-center text-slate-400";
-								}
-							} else {
-								return "p-2 text-center text-slate-300";
-							}
-						})()}
-					>
-						{level ? "〇" : "×"}
-					</td>
-				)}
-			</For>
-		</tr>
-	);
+  return (
+    <tr class={has() ? "border-b bg-slate-100" : "border-b"}>
+      <td class="group relative p-2">
+        <Show when={0 < hints().length}>
+          <Hints />
+        </Show>
+      </td>
+      <td
+        class="cursor-pointer p-2 text-center"
+        onClick={props.onClick}
+        tabindex={0}
+      >
+        <HiOutlinePlusCircle class="inline" color="#444444" />
+        {course().name}
+      </td>
+      <td class="p-2 text-center">{course().groups.get(1)}</td>
+      <td class="p-2 text-center">{course().groups.get(2)}</td>
+      <td class="p-2 text-center">{course().unit}</td>
+      <For each={Array.from(course().levels)}>
+        {([key, level]) => (
+          <td
+            class={(() => {
+              if (level) {
+                if (props.point() == key) {
+                  return "p-2 text-center font-bold text-purple-600";
+                } else if (props.point() < key) {
+                  return "p-2 text-center text-purple-600";
+                } else {
+                  return "p-2 text-center text-slate-400";
+                }
+              } else {
+                return "p-2 text-center text-slate-300";
+              }
+            })()}
+          >
+            {level ? "〇" : "×"}
+          </td>
+        )}
+      </For>
+    </tr>
+  );
 };
 
 const CourseTable: Component = () => {
-	const table = useTable();
+  const table = useTable();
 
-	const [point, setPoint] = createSignal(4);
-	const [tagSet, setTagSet] = createSignal(new Set<string>());
+  const [point, setPoint] = createSignal(4);
+  const [tagSet, setTagSet] = createSignal(new Set<string>());
 
-	createEffect(() => {
-		if (!table.diagnostics()) return;
-		setTagSet(new Set(table.diagnostics().tagArray));
-	});
+  createEffect(() => {
+    if (!table.diagnostics()) return;
+    setTagSet(new Set(table.diagnostics().tagArray));
+  });
 
-	const hasTag = (tag: string) => tagSet().has(tag);
-	const toggleTag = (tag: string) => {
-		const _tagSet = new Set(tagSet());
-		if (_tagSet.has(tag)) _tagSet.delete(tag);
-		else _tagSet.add(tag);
-		setTagSet(_tagSet);
-	};
+  const hasTag = (tag: string) => tagSet().has(tag);
+  const toggleTag = (tag: string) => {
+    const _tagSet = new Set(tagSet());
+    if (_tagSet.has(tag)) _tagSet.delete(tag);
+    else _tagSet.add(tag);
+    setTagSet(_tagSet);
+  };
 
-	return (
-		<Show
-			when={table.complete()}
-			fallback={
-				<div class="flex justify-center">
-					<div class="h-10 w-10 animate-spin rounded-full border-4 border-purple-600 border-t-transparent" />
-				</div>
-			}
-		>
-			{/* tag filter */}
-			<div class="mb-4 text-center">
-				<span class="font-bold mx-2">タグ</span>
-				<For each={table.diagnostics().tagArray}>
-					{(tag) => (
-						<span
-							tabindex={0}
-							class={
-								hasTag(tag)
-									? "mx-2 cursor-pointer rounded-lg bg-slate-100 p-2"
-									: "mx-2 cursor-pointer rounded-lg p-2"
-							}
-							onClick={() => toggleTag(tag)}
-						>
-							{tag}
-						</span>
-					)}
-				</For>
-			</div>
+  return (
+    <Show
+      when={table.complete()}
+      fallback={
+        <div class="flex justify-center">
+          <div class="h-10 w-10 animate-spin rounded-full border-4 border-purple-600 border-t-transparent" />
+        </div>
+      }
+    >
+      {/* tag filter */}
+      <div class="mb-4 text-center">
+        <span class="mx-2 font-bold">タグ</span>
+        <For each={table.diagnostics().tagArray}>
+          {(tag) => (
+            <span
+              tabindex={0}
+              class={
+                hasTag(tag)
+                  ? "mx-2 cursor-pointer rounded-lg bg-slate-100 p-2"
+                  : "mx-2 cursor-pointer rounded-lg p-2"
+              }
+              onClick={() => toggleTag(tag)}
+            >
+              {tag}
+            </span>
+          )}
+        </For>
+      </div>
 
-			{/* highlight */}
-			<div class="mb-4 text-center">
-				<span class="font-bold mx-2">ハイライト</span>
-				<select
-					class="mx-2 rounded-lg bg-slate-100 p-2"
-					value={point()}
-					onChange={(e) => setPoint(Number(e.currentTarget.value))}
-				>
-					<option value={1}>1年前期</option>
-					<option value={2}>1年後期</option>
-					<option value={3}>2年前期</option>
-					<option value={4}>2年後期</option>
-					<option value={5}>3年前期</option>
-					<option value={6}>3年後期</option>
-					<option value={7}>4年前期</option>
-					<option value={8}>4年後期</option>
-				</select>
-			</div>
+      {/* highlight */}
+      <div class="mb-4 text-center">
+        <span class="mx-2 font-bold">ハイライト</span>
+        <select
+          class="mx-2 rounded-lg bg-slate-100 p-2"
+          value={point()}
+          onChange={(e) => setPoint(Number(e.currentTarget.value))}
+        >
+          <option value={1}>1年前期</option>
+          <option value={2}>1年後期</option>
+          <option value={3}>2年前期</option>
+          <option value={4}>2年後期</option>
+          <option value={5}>3年前期</option>
+          <option value={6}>3年後期</option>
+          <option value={7}>4年前期</option>
+          <option value={8}>4年後期</option>
+        </select>
+      </div>
 
-			{/* table */}
-			<table class="mx-auto overflow-auto border">
-				<thead>
-					<tr class="border-b">
-						<th class="p-2 text-center">解析</th>
-						<th class="p-2 text-center">講義名</th>
-						<th class="p-2 text-center">区分1</th>
-						<th class="p-2 text-center">区分2</th>
-						<th class="p-2 text-center">単位数</th>
-						<th class="p-2 text-center">1年前期</th>
-						<th class="p-2 text-center">1年後期</th>
-						<th class="p-2 text-center">2年前期</th>
-						<th class="p-2 text-center">2年後期</th>
-						<th class="p-2 text-center">3年前期</th>
-						<th class="p-2 text-center">3年後期</th>
-						<th class="p-2 text-center">4年前期</th>
-						<th class="p-2 text-center">4年後期</th>
-					</tr>
-				</thead>
-				<tbody>
-					<Index each={table.courseArray()}>
-						{(course) => (
-							<CourseTableRow
-								course={course}
-								tagSet={tagSet}
-								point={point}
-								onClick={() => table.toggleHasCourse(course().key)}
-							/>
-						)}
-					</Index>
-				</tbody>
-			</table>
-		</Show>
-	);
+      {/* table */}
+      <table class="mx-auto overflow-auto border">
+        <thead>
+          <tr class="border-b">
+            <th class="p-2 text-center">解析</th>
+            <th class="p-2 text-center">講義名</th>
+            <th class="p-2 text-center">区分1</th>
+            <th class="p-2 text-center">区分2</th>
+            <th class="p-2 text-center">単位数</th>
+            <th class="p-2 text-center">1年前期</th>
+            <th class="p-2 text-center">1年後期</th>
+            <th class="p-2 text-center">2年前期</th>
+            <th class="p-2 text-center">2年後期</th>
+            <th class="p-2 text-center">3年前期</th>
+            <th class="p-2 text-center">3年後期</th>
+            <th class="p-2 text-center">4年前期</th>
+            <th class="p-2 text-center">4年後期</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Index each={table.courseArray()}>
+            {(course) => (
+              <CourseTableRow
+                course={course}
+                tagSet={tagSet}
+                point={point}
+                onClick={() => table.toggleHasCourse(course().key)}
+              />
+            )}
+          </Index>
+        </tbody>
+      </table>
+    </Show>
+  );
 };
 
 const App: Component = () => {
-	return (
-		<div class="mx-auto min-w-[1280px] py-5">
-			<div class="mb-8">
-				<h1 class="mb-3 text-center text-4xl">Uny📎</h1>
-				<p class="text-center">獲得単位が条件を満たすか確認します</p>
-			</div>
-			<TableProvider>
-				<CourseTable />
-			</TableProvider>
-		</div>
-	);
+  return (
+    <div class="mx-auto min-w-[1280px] py-5">
+      <div class="mb-8">
+        <h1 class="mb-3 text-center text-4xl">Uny📎</h1>
+        <p class="text-center">獲得単位が条件を満たすか確認します</p>
+      </div>
+      <TableProvider>
+        <CourseTable />
+      </TableProvider>
+    </div>
+  );
 };
 
 export default App;
